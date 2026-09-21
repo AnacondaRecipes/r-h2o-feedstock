2efcc06ac24c67bfb1c31d7325f3069a4ea6e644
Andy Maloney <amaloney@mailbox.org>
2026-09-21 17:18:23 -0500

    fix cmd

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,10 @@ set -e
 
 # H2O release zip has R package tarball in R/ directory
 R_PKG=$(ls "$SRC_DIR"/R/h2o_*.tar.gz 2>/dev/null | head -1)
-$R CMD INSTALL "$R_PKG" -l "$PREFIX/lib/R/library"
+if [ -z "$R_PKG" ]; then
+    echo "ERROR: no h2o_*.tar.gz found under $SRC_DIR/R" >&2
+    exit 1
+fi
+
+R="${R:-$PREFIX/bin/R}"
+"$R" CMD INSTALL "$R_PKG" -l "$PREFIX/lib/R/library"
